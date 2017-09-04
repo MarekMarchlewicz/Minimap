d3ed2ab8a3ba252df31482a3eb9e4ca6a919ed65
Marek <marek.marchlewicz@gmail.com>
2017-09-04 09:51:01 +0800

    Added rotation to the minimap

--- Minimap/Assets/Scripts/MinimapCamera.cs
+++ Minimap/Assets/Scripts/MinimapCamera.cs
@@ -22,10 +22,16 @@ public class MinimapCamera : MonoBehaviour
 	private void LateUpdate()
 	{
 		Vector3 newPosition = transform.position;
-
 		newPosition.x = targetToFollow.position.x;
 		newPosition.z = targetToFollow.position.z;
 
+        Vector3 newRotation = targetToFollow.rotation.eulerAngles;
+        newRotation.x = 0f;
+        newRotation.z = 0f;
+        Debug.Log(newRotation.y);
+
 		transform.position = newPosition;
+
+        transform.rotation = Quaternion.Euler(newRotation) * Quaternion.Euler(Vector3.right * 90);
 	}
 }
